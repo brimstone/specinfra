003a3b04d645ed81c9af71f82197a39580c84b66
Matt <brimstone@the.narro.ws>
2015-05-26 17:11:29 -0400

    Switched to exactly what base/port uses, except for using ss intead of netstat.

--- lib/specinfra/command/linux/base/port.rb
+++ lib/specinfra/command/linux/base/port.rb
@@ -1,15 +1,10 @@
 class Specinfra::Command::Linux::Base::Port < Specinfra::Command::Base::Port
   class << self
     def check_is_listening(port, options = {})
-      options[:local_address] = '*' unless options[:local_address]
-      laddr = "#{options[:local_address]}:#{port}"
-      if options[:protocol] == 'tcp'
-        "ss -tnl | awk 'BEGIN {c=0} $4 == \"#{laddr}\" {c++} END { if (c>0) {exit(0)}; exit(1) }'"
-      elsif options[:protocol] == 'udp'
-        "ss -unl | awk 'BEGIN {c=0} $4 == \"#{laddr}\" {c++} END { if (c>0) {exit(0)}; exit(1) }'"
-      else
-        "ss -nl | awk 'BEGIN {c=0} $5 == \"#{laddr}\" {c++} END { if (c>0) {exit(0)}; exit(1) }'"
-      end
+      pattern = ":#{port} "
+      pattern = " #{options[:local_address]}#{pattern}" if options[:local_address]
+      pattern = "^#{options[:protocol]} .*#{pattern}" if options[:protocol]
+      "ss -tunl | grep -- #{escape(pattern)}"
     end
   end
 end
